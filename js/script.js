01623e68b69a321c028fb761614644f30b945435
Joram Livengood <joram.livengood@gmail.com>
2020-06-17 17:16:28 -0500

    Events and changes
    Themes event listener, show and hide colors based on theme chosen

--- js/script.js
+++ js/script.js
@@ -3,14 +3,11 @@ const nameField = document.getElementById('name');                      // grab 
 const titleField = document.getElementById('title');                    // grab title select field
 const otherTitleLabel = document.getElementById('other-label');         // grab other label
 const otherTitleField = document.getElementById('other-title');         // grab other title field
-const selectShirt = document.createElement('option');                   // create initial color option "Please select a
-                                                                          // T-shirt theme"
+const themeOptions = document.getElementById('design');                  // grab color options
 const colorOptions = document.getElementById('color');                  // grab color options
 const colorOptionElements = document.querySelectorAll('#color option'); // grab all color options
-
-
-selectShirt.text = 'Please select a T-shirt theme'; // set the text of new option
-colorOptions.add(selectShirt, colorOptions[0]);     // and new option to the beginning of option list
+const colorLabel = document.querySelector('label[for="color"]');
+colorLabel.textContent = 'Please select a T-shirt theme';
 
 
 // ========== INITIAL STATES ========== //
@@ -21,9 +18,14 @@ otherTitleField.style.display = 'none';  // hide other title field
 
 
 // ========== LOOPS ========== //
-for (i = 0; i < colorOptionElements.length; i++) { // disable option list items
+for (let i = 0; i < colorOptionElements.length; i++) { // disable option list items
   colorOptionElements[i].disabled = true;
-  //console.log(i);
+
+  if (i <= 2) {
+    colorOptionElements[i].classList.add('puns');
+  } else {
+    colorOptionElements[i].classList.add('hearts');
+  }
 }
 
 
@@ -42,6 +44,29 @@ window.addEventListener('change', (e) => {    // add change listen to title sele
     } else {                                  // else if the target value is NOT 'other'
       otherTitleLabel.style.display = 'none'; // hide other label
       otherTitleField.style.display = 'none'; // hide other text field
+    }
+  }
+});
+
+window.addEventListener('change', (e) => {
+  let target = e.target;
+  const puns = document.getElementsByClassName('puns');
+  const hearts = document.getElementsByClassName('hearts');
+  colorLabel.textContent = 'Color:';
+
+  if (target === themeOptions) {
+    if (target.value === 'js puns') {
+      for (let i = 0; i < 3; i++) {
+        puns[i].disabled = false;
+        puns[i].style.display = '';
+        hearts[i].style.display = 'none';
+      }
+    } else if (target.value === 'heart js') {
+      for (let i = 0; i < 3; i++) {
+        hearts[i].disabled = false;
+        hearts[i].style.display = '';
+        puns[i].style.display = 'none';
+      }
     }
   }
 });
